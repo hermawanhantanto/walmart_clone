74b9f0ce8bd9e0e3b8e641d0cbc13c0e32833d26
Hermawan Hantanto <hermawan.hant@gmail.com>
2024-06-29 20:56:06 +0700

    fix

--- scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Install Node.js, PM2, and other necessary dependencies
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-sudo yum install -y nodejs
-sudo npm install -g pm2
